c31676be2be7ea78e2fbc62d03886b993129eebc
Emelit0 <stollk811@gmail.com>
2023-05-02 23:07:22 +0200

    basic sturcture

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void header() {
+    printf("IP CALCULATOR");
+    printf("Input a IP Address and the Subnet Mask!");
+    printf("Calculate the possible amount of hosts and subnets of the IP given");
+    printf("can also output the given CIDR mask into decimal");
+}
+
+int main() {
+    int IP1, IP2, IP3, IP4, CIDR;
+    int bcIP1, bcIP2, bcIP3, bcIP4;
+    int IPgiven = 0;
+    int CIDRgiven = 0;
+    double aHosts;
+
+    header();
+}
+
