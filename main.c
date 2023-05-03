2d1af19b9f15a71d96a7168d8aa78ac0cfc2b3bc
Emelit0 <stollk811@gmail.com>
2023-05-03 23:51:30 +0200

    new struct and calculate subnet_mask, network_address, broadcast_address & number of hosts

--- main.c
+++ main.c
@@ -8,25 +8,56 @@ void header() {
 }
 
 int main() {
-    int IP1, IP2, IP3, IP4, CIDR;
-    int bcIP1, bcIP2, bcIP3, bcIP4;
-    int IPgiven = 0;
-    int CIDRgiven = 0;
-    double aHosts;
 
-    header();
+    typedef struct {
+        uint32_t octet1 : 8;
+        uint32_t octet2 : 8;
+        uint32_t octet3 : 8;
+        uint32_t octet4 : 8;
+    } ipv4_addr;
 
-    while (IPgiven == 0) {
-        printf("/n Please input a IP-address in the right format XXX.XXX.XXX.XXX: ");
-        scanf("%i.%i.%i.%i.%i", &IP1, &IP2, &IP3, &IP4);
+    typedef struct {
+        ipv4_addr network_addr;
+        ipv4_addr subnet_mask;
+        ipv4_addr broadcast_addr;
+        uint32_t num_hosts;
+    } subnet_info;
 
-        if (IP1 > 255 || IP2 > 255 || IP3 > 255 || IP4 > 255 || IP1 == 127 || IP1 == 255 || IP1 < 1 || IP2 < 0 || IP3 < 0 || IP4 < 0) {
-            printf("Please validate your input and check for the correct format");
+    void get_subnet_info(ipv4_addr ip_addr, uint8_t mask_len, subnet_info* subnet) {
 
-        } else {
-            IPgiven = 1;
+        // Calculate subnet mask
+        uint32_t subnet_mask = 0;
+        for (int i = 0; i < mask_len; i++) {
+            subnet_mask |= (1 << (31 - i)); // set the i-th bit of subnet mask to 1
         }
+        subnet->subnet_mask.octet1 = (subnet_mask >> 24) & 0xff;
+        subnet->subnet_mask.octet2 = (subnet_mask >> 16) & 0xff;
+        subnet->subnet_mask.octet3 = (subnet_mask >> 8) & 0xff;
+        subnet->subnet_mask.octet4 = subnet_mask & 0xff;
 
+        // Calculate network address
+        subnet->network_addr.octet1 = ip_addr.octet1 & subnet->subnet_mask.octet1;
+        subnet->network_addr.octet2 = ip_addr.octet2 & subnet->subnet_mask.octet2;
+        subnet->network_addr.octet3 = ip_addr.octet3 & subnet->subnet_mask.octet3;
+        subnet->network_addr.octet4 = ip_addr.octet4 & subnet->subnet_mask.octet4;
+
+        // Calculate broadcast address
+        subnet->broadcast_addr.octet1 = subnet->network_addr.octet1;
+        subnet->broadcast_addr.octet2 = subnet->network_addr.octet2;
+        subnet->broadcast_addr.octet3 = subnet->network_addr.octet3;
+        subnet->broadcast_addr.octet4 = subnet->network_addr.octet4;
+
+        // Calculate number of hosts
+        uint32_t num_hosts = (1 << (32 - mask_len)) - 2;
+        for (int i = 0; i < 4; i++) {
+            subnet->broadcast_addr.octet4 |= (1 << i); // set the i-th bit of broadcast address to 1
+            if (subnet->broadcast_addr.octet4 > subnet->network_addr.octet4) {
+                subnet->num_hosts = num_hosts;
+                break;
+            }
+            subnet->broadcast_addr.octet4 &= ~(1 << i); // set the i-th bit of broadcast address to 0
+        }
     }
+
 }
 
